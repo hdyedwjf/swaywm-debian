7345dd83cfbcfa3c8201a4f4c2a41bf0b0ba1836
hdyedwjf <137915981+hdyedwjf@users.noreply.github.com>
2024-06-20 20:41:46 +0800

    Add files via upload

---
+++ swaydebian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install sway swaylock firefox-esr tlp fonts-wqy-zenhei thunar xarchiver thunar-archive-plugin flatpak xfce4-terminal \
+pipewire alsa-utils fcitx5-chinese-addons fcitx5 mpv
